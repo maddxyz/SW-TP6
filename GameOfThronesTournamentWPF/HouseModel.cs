47f5aee5a4e1e72aaad65af628b0fc2fd3e6ae35
Imad E <enneiymy.i@gmail.com>
2018-03-09 15:56:30 +0100

    Main-Window + Local test of ShowHouses_Click

---
+++ GameOfThronesTournamentWPF/HouseModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfThronesTournamentWPF
+{
+    class HouseModel
+    {
+
+        public string Name { get; set; }
+        public int NbUnits { get; set; }
+
+        public HouseModel() { }
+
+        public override string ToString()
+        {
+            return "Name: " + Name + " Units: " + NbUnits;
+        }
+
+
+    }
+}
---
+++ GameOfThronesTournamentWPF/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GameOfThronesTournamentWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ShowHouses_Click(object sender, RoutedEventArgs e)
+        {
+            List<HouseModel> l = new List<HouseModel>();
+
+            l.Add(new HouseModel() { Name = "I", NbUnits = 450 });
+            l.Add(new HouseModel() { Name = "Y", NbUnits = 231 });
+
+            ResList.ItemsSource = l;
+        }
+
+        private void ShowChars_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ShowWars_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Sho(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
