c2bab97a7a952b0e35a7e557ed531f657a1b471d
Yassir Karroum <ukarroum17@gmail.com>
2018-03-16 12:44:05 +0100

    Add error handling (kind of the app just close)

--- GameOfThronesTournamentWPF/App.xaml.cs
+++ GameOfThronesTournamentWPF/App.xaml.cs
@@ -13,5 +13,17 @@ namespace GameOfThronesTournamentWPF
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            DispatcherUnhandledException += App_DispatcherUnhandledException;
+        }
+
+        void App_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show("Application is going to close! ", "Uncaught Exception");
+            e.Handled = true;
+            Application.Current.Shutdown();
+        }
     }
 }
